73904d8d0fa45a8b224551a3c75f6d3293de960f
YasteneAK <121824763+YasteneAK@users.noreply.github.com>
2023-02-18 21:09:59 +0100

    Create Cloud.js

---
+++ src/components/cloud/Cloud.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./CloudStyles.css";
+
+const Cloud = () => {
+  return (
+    <div className="cloud">
+      <div className="container">
+        <div className="content">
+          <h2>
+            <span>Cloud</span> Security
+          </h2>
+          <p>
+            Cloud Security solutions to protect data and apps. Our experts use
+            cutting-edge technology to secure your cloud environment. Trust us
+            to keep your data safe, enabling business growth.
+          </p>
+          <div>
+            <button>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cloud;
