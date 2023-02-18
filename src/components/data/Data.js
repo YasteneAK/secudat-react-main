99be03c968f7285299d09790fd4bf1984cb17faa
YasteneAK <121824763+YasteneAK@users.noreply.github.com>
2023-02-18 21:13:46 +0100

    Create Data.js

---
+++ src/components/data/Data.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./DataStyles.css";
+
+const Data = () => {
+  return (
+    <div className="data">
+      <div className="container">
+        <div className="content">
+          <h2>
+            <span>Data</span> Recovery
+          </h2>
+          <p>
+            Data Recovery services to restore lost, damaged, or inaccessible
+            data. Our experts use advanced technology to recover data from
+            various storage devices. Fast, reliable, and secure solutions to
+            retrieve valuable data and ensure peace of mind.
+          </p>
+          <div>
+            <button>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Data;
