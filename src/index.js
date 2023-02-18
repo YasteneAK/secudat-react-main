d1b0f7e786717e4b5a09e6e54af435de0717f791
YasteneAK <121824763+YasteneAK@users.noreply.github.com>
2023-02-18 21:08:25 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Recovery from './routes/RecoveryPage'
+import CloudPage from './routes/CloudPage'
+import ContactPage from './routes/ContactPage'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/recovery' element={<Recovery />} />
+      <Route path='/cloud' element={<CloudPage />} />
+      <Route path='/contact' element={<ContactPage />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
