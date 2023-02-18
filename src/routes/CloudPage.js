119e9b5a7fe120a0ae725d7ad82500ef7915596e
YasteneAK <121824763+YasteneAK@users.noreply.github.com>
2023-02-18 21:23:49 +0100

    Create CloudPage.js

---
+++ src/routes/CloudPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Navbar from "../components/navbar/Navbar";
+import Cloud from "../components/cloud/Cloud";
+import Footer from "../components/footer/Footer";
+
+const CloudPage = () => {
+  return (
+    <>
+      <Navbar />
+      <Cloud />
+
+      <Footer />
+    </>
+  );
+};
+
+export default CloudPage;
