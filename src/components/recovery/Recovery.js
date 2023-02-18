1722c1b64b18ca2ffde73d2188fb1b82d9a4675a
YasteneAK <121824763+YasteneAK@users.noreply.github.com>
2023-02-18 21:21:06 +0100

    Create Recovery.js

---
+++ src/components/recovery/Recovery.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./RecoveryStyles.css";
+
+const Recovery = () => {
+  return (
+    <div className="recovery">
+      <div className="container">
+        <div className="content">
+          <h2>
+            <span>Data</span> Recovery
+          </h2>
+          <p>
+            Data Recovery services to restore lost, damaged, or inaccessible
+            data. Our experts use advanced technology to recover data from
+            various storage devices. Fast, reliable, and secure solutions to
+            retrieve valuable data and ensure peace of mind.
+          </p>
+          <div>
+            <button>Learn More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recovery;
